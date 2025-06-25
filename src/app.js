9aa733a9700dc536e540d53cbacd2977d2b8ca9f
Ti Alejandria <tisistemas.alejandria@gmail.com>
2025-06-25 12:14:08 -0500

    Habiltado el CORS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express=require('express')
+const cors=require('cors')
 const routerForm = require('./routes/forms')
 const pageViewRouter = require('./routes/pageView')
 const apiRouter=express.Router()
@@ -7,6 +8,8 @@ require('dotenv').config()
 const app=express()
 const port=process.env.PORT
 
+
+app.use(cors())
 app.use(express.json())
 app.use('/api',apiRouter)
 
